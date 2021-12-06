c344b3092f257eb8fa6638e0fb069095a5b3cf67
gregHernandez <gregoryarthur13@gmail.com>
2021-12-05 21:04:51 -0600

    feat: routes added
    create and discount routes added

--- routes/payment.js
+++ routes/payment.js
@@ -4,9 +4,11 @@ var paymentController = require('../controllers/payment.controller');
 var middleware = require('../middleware');
 
 
-//router.use(middleware);
+router.use(middleware);
 
 router
+    .post('/', paymentController.create)
+    .post('/discount', paymentController.applyDiscount)
     .get('/promos', paymentController.getPromos);
 
 module.exports = router;
